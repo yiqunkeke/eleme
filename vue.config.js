2f88db464a044174b2bfb83d6e002bfdea444f7f
coco <1431806069@qq.com>
2019-06-15 19:08:04 +0800

    cube-ui插件安装和通过devServer定义接口来mock数据

---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    // 所有 webpack-dev-server 的选项都支持
+    before(app, server) {
+      app.get('/api/seller', function(req, res){
+        res.json({
+          code: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function(req, res){
+        res.json({
+          code: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function(req, res){
+        res.json({
+          code: 0,
+          data: ratings
+        })
+      })
+    }
+  }
+}
